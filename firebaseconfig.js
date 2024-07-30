16ec2994975ef8c613a9d4b4ec1b441c28b1906d
developerfahad1 <fahadgraphic.pk>
2024-07-30 15:37:27 +0500

    firebase date store

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAOKIu9h0OZx5-uKP7-CDMeCxyHvqVHU8Y",
@@ -14,3 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
--- home.js
+++ home.js
@@ -1,5 +1,13 @@
 import { getAuth, onAuthStateChanged , signOut   } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./firebaseconfig.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { db } from "./firebaseconfig.js";
+
+
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -21,3 +29,53 @@ onAuthStateChanged(auth, (user) => {
       });
   })
   
+
+  
+
+  const form = document.querySelector("#form");
+  const todo = document.querySelector("#todo");
+  const ul = document.querySelector("#ul");
+  
+  const arr = [];
+  
+  async function getData() {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      arr.push(doc.data());
+    });
+    console.log(arr);
+    renderTodo();
+  }
+  
+  getData();
+  
+  function renderTodo() {
+    ul.innerHTML = "";
+    if (arr.length === 0) {
+      ul.innerHTML = "no data found";
+      return;
+    }
+    arr.map((item) => {
+      ul.innerHTML += `
+          <li>${item.todo}</li>
+          `;
+    });
+  }
+  
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    arr.push({
+      todo: todo.value,
+    });
+    renderTodo();
+    try {
+      const docRef = await addDoc(collection(db, "todos"), {
+        todo: todo.value,
+      });
+      todo.value = "";
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  });
